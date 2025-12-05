153c0e37129edcbfdbe80be9c934f90ec59b8cfb
quocthai07 <dongquocthai.31082006@gmail.com>
2025-12-05 21:41:54 +0700

    Add class City + Flight

--- Models/City.cs
+++ Models/City.cs
@@ -2,5 +2,15 @@
 
 public class City
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Country { get; set; }
+    public string AirportCode { get; set; }
+    public float Latitude { get; set; }
+    public float Longitude { get; set; }
+
+    public string GetInfo()
+    {
+        return $"{Name} ({AirportCode}), {Country}";
+    }
 }
--- Models/Flight.cs
+++ Models/Flight.cs
@@ -2,5 +2,10 @@
 
 public class Flight
 {
-    
+    public int Id { get; set; };
+    public int SourceCityId { get; set; };
+    public int DestCityId { get; set; };
+    public float Price { get; set; };
+    public int Duration { get; set; }; // minutes
+    public string Airline { get; set; };
 }
